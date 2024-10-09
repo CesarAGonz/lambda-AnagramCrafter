3fdcbf08a1eb5ea94dc058e913965b6252a2262a
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-08 20:55:33 -0600

    Anagrams func

---
+++ index.py
@@ -0,0 +1,20 @@
+import json
+
+def are_anagrams(string1, string2):
+    string1 = ''.join(sorted(string1.replace(" ", "").lower()))
+    string2 = ''.join(sorted(string2.replace(" ", "").lower()))
+    return string1 == string2
+
+def lambda_handler(event, context):
+    string1 = event.get('string1', '')
+    string2 = event.get('string2', '')
+
+    result = are_anagrams(string1, string2)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'areAnagrams': result,
+            'message': f"'{string1}' and '{string2}' are anagrams: {result}"
+        })
+    }
