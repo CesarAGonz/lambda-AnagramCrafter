28ea4021e4d9dd8a9e80e3d9ac98245d0bdb2a83
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-09 02:13:55 -0600

    Rename vars

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,20 @@
 import json
 
 def are_anagrams(string1, string2):
-    string1 = ''.join(sorted(string1.replace(" ", "").lower()))
-    string2 = ''.join(sorted(string2.replace(" ", "").lower()))
-    return string1 == string2
+    clnd_strg1 = ''.join(sorted(string1.replace(" ", "").lower()))
+    clnd_strg2 = ''.join(sorted(string2.replace(" ", "").lower()))
+    return clnd_strg1 == clnd_strg2
 
 def lambda_handler(event, context):
-    string1 = event.get('string1', '')
-    string2 = event.get('string2', '')
-
-    result = are_anagrams(string1, string2)
+    org_strg1 = event.get('string1', '')
+    org_strg2 = event.get('string2', '')
+    
+    result = are_anagrams(org_strg1, org_strg2)
     
     return {
         'statusCode': 200,
         'body': json.dumps({
             'areAnagrams': result,
-            'message': f"'{string1}' and '{string2}' are anagrams?: {result}"
+            'message': f"'{org_strg1}' and '{org_strg2}' are anagrams?: {result}"
         })
     }
